8ad6e8374354b953febaecb7a4616a6288e9376e
Sinuhe Jaime <sinuhe.jaimevalencia@cognizant.com>
2018-07-04 01:37:08 -0500

    Parser dependency moved to a different place

--- app/src/main/java/com/gdlactivity/gdggithub/app/GDGApp.kt
+++ app/src/main/java/com/gdlactivity/gdggithub/app/GDGApp.kt
@@ -2,16 +2,19 @@ package com.gdlactivity.gdggithub.app
 
 import android.app.Application
 import com.gdlactivity.gdggithub.api.GithubService
-import com.squareup.moshi.Moshi
+import com.gdlactivity.gdggithub.di.ParserModule
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 class GDGApp : Application() {
-    val jsonParser: Moshi = Moshi.Builder().build()
+    val parserModule = ParserModule()
 
     val restClient: Retrofit = Retrofit.Builder()
         .baseUrl("https://api.github.com")
-        .addConverterFactory(MoshiConverterFactory.create(jsonParser))
+        .addConverterFactory(
+            parserModule.provideMoshiParserFactory(
+                parserModule.provideMoshiParser()
+            )
+        )
         .build()
 
     val githubService: GithubService = restClient.create(GithubService::class.java)
---
+++ app/src/main/java/com/gdlactivity/gdggithub/di/ParserModule.kt
@@ -0,0 +1,15 @@
+package com.gdlactivity.gdggithub.di
+
+import com.squareup.moshi.Moshi
+import retrofit2.Converter
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class ParserModule {
+    fun provideMoshiParserFactory(moshi: Moshi): Converter.Factory {
+        return MoshiConverterFactory.create(moshi)
+    }
+
+    fun provideMoshiParser(): Moshi {
+        return Moshi.Builder().build()
+    }
+}
